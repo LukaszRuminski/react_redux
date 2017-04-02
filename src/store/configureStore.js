af001851a11786f6bb1ad2354cf211bf6084c2da
Lukasz Ruminski <ruminski.lukasz@gmail.com>
2017-04-02 20:34:32 +0200

    dynamic menu phase 1

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,11 +1,13 @@
 import {createStore, applyMiddleware} from 'redux';
 import rootReducer from '../reducers';
 import reduxImmutableStateInvriant from 'redux-immutable-state-invariant';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
 
 export default function configureStore(initialState){
   return createStore(
     rootReducer,
     initialState,
-    applyMiddleware(reduxImmutableStateInvriant())
+    composeWithDevTools(applyMiddleware(reduxImmutableStateInvriant()))
   );
 }
